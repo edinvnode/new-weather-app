06beccc72c79fd8f4fb861f38269a26734ba209b
Edin <edin.vatrich@gmail.com>
2025-04-21 19:12:56 +0200

    Addeed three day forecast

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { useEffect, useState } from 'react';
-
-const API_KEY = '66fc738f52b19a4f2eadf7b062f38778';
+import React, { useState } from 'react';
 
 function App() {
   const [weather, setWeather] = useState(null);
+  const [temperatures, setTemperatures] = useState(null);
   const [city, setCity] = useState('');
+  const [newCity, setNewCity] = useState('');
+  //const [lat, setLat] = useState(0);
+  //const [lon, setLon] = useState(0);
   const [error, setError] = useState('');
   const [errorMessage, setErrorMessage] = useState(false);
+
+  const [tempError, setTempError] = useState('');
+  const [tempErrorMessage, seTempErrorMessage] = useState(false);
+
+  const API_KEY = '66fc738f52b19a4f2eadf7b062f38778';
 
   async function fetchData(e) {
     e.preventDefault();
     setWeather(null);
 
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${newCity}&cnt=3&appid=${API_KEY}&units=metric`
     );
     if (!response.ok) {
       //throw new Error('City not found');
@@ -25,6 +32,29 @@ function App() {
       setWeather(data);
       setError('');
       setErrorMessage(false);
+    }
+
+    //console.log(data);
+  }
+
+  async function fetchTemperatures(e) {
+    e.preventDefault();
+    setTemperatures(null);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${newCity}&cnt=3&appid=${API_KEY}&units=metric`
+    );
+    if (!response.ok) {
+      //throw new Error('City not found');
+      setTempError('City not found.');
+      seTempErrorMessage(true);
+    } else {
+      const data = await response.json();
+
+      setTemperatures(data);
+      setTempError('');
+      seTempErrorMessage(false);
+      console.log(data);
     }
 
     //console.log(data);
@@ -43,6 +73,18 @@ function App() {
         />
         <button type="submit">Submit</button>
       </form>
+
+      <form onSubmit={fetchTemperatures}>
+        <label>Enter city name for temperatures search</label>
+        <input
+          type="text"
+          className="city-input"
+          placeholder="Enter city name..."
+          onChange={(e) => setNewCity(e.target.value)}
+          value={newCity}
+        />
+        <button type="submit">Submit</button>
+      </form>
       {!errorMessage && (
         <div className="weather-data">
           <p>
@@ -52,11 +94,25 @@ function App() {
           <p>Weather: {weather?.weather[0]?.main}</p>
           <p>Wind: {weather?.wind?.speed} m/s</p>
           <p>Humidity: {weather?.main?.humidity} %</p>
+          <p>Longitude: {weather?.coord?.lon}</p>
+          <p>Latitude: {weather?.coord?.lat}</p>
         </div>
       )}
       {errorMessage && (
         <div className="error-message">
           <span>{error}</span>
+        </div>
+      )}
+
+      {!tempErrorMessage && temperatures && (
+        <div className="weather-data">
+          {temperatures.list.slice(0, 3).map((item, index) => (
+            <div key={index}>
+              <p>Forecast time: {new Date(item.dt * 1000).toLocaleString()}</p>
+              <p>Temperature: {item.main.temp} °C</p>
+              <p>Weather: {item.weather[0].main}</p>
+            </div>
+          ))}
         </div>
       )}
     </div>
