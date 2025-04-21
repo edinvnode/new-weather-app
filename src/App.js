4f98e12fe7fd6bddd508ae4f089aab97731b1fda
Edin <edin.vatrich@gmail.com>
2025-04-21 19:29:00 +0200

    Fixed issues

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     setWeather(null);
 
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${newCity}&cnt=3&appid=${API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     );
     if (!response.ok) {
       //throw new Error('City not found');
@@ -32,9 +32,8 @@ function App() {
       setWeather(data);
       setError('');
       setErrorMessage(false);
+      console.log(data);
     }
-
-    //console.log(data);
   }
 
   async function fetchTemperatures(e) {
@@ -54,7 +53,7 @@ function App() {
       setTemperatures(data);
       setTempError('');
       seTempErrorMessage(false);
-      console.log(data);
+      //console.log(data);
     }
 
     //console.log(data);
@@ -62,59 +61,76 @@ function App() {
 
   return (
     <div className="App">
-      <form onSubmit={fetchData}>
-        <label>Enter city name</label>
-        <input
-          type="text"
-          className="city-input"
-          placeholder="Enter city name..."
-          onChange={(e) => setCity(e.target.value)}
-          value={city}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <div className="input-container">
+        <form onSubmit={fetchData}>
+          <label>Enter city name</label>
+          <input
+            type="text"
+            className="city-input"
+            placeholder="Enter city name..."
+            onChange={(e) => setCity(e.target.value)}
+            value={city}
+          />
+          <button type="submit">Submit</button>
+        </form>
 
-      <form onSubmit={fetchTemperatures}>
-        <label>Enter city name for temperatures search</label>
-        <input
-          type="text"
-          className="city-input"
-          placeholder="Enter city name..."
-          onChange={(e) => setNewCity(e.target.value)}
-          value={newCity}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      {!errorMessage && (
-        <div className="weather-data">
-          <p>
-            City {weather?.name} , Country: {weather?.sys?.country}
-          </p>
-          <p>Temperature: {weather?.main?.temp} °C</p>
-          <p>Weather: {weather?.weather[0]?.main}</p>
-          <p>Wind: {weather?.wind?.speed} m/s</p>
-          <p>Humidity: {weather?.main?.humidity} %</p>
-          <p>Longitude: {weather?.coord?.lon}</p>
-          <p>Latitude: {weather?.coord?.lat}</p>
-        </div>
-      )}
-      {errorMessage && (
-        <div className="error-message">
-          <span>{error}</span>
-        </div>
-      )}
+        <form onSubmit={fetchTemperatures}>
+          <label>Enter city for daily forecast</label>
+          <input
+            type="text"
+            className="city-input"
+            placeholder="Enter city name..."
+            onChange={(e) => setNewCity(e.target.value)}
+            value={newCity}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div className="weather-container">
+        {!errorMessage && weather && (
+          <div className="weather-data">
+            <p>
+              City: {weather.name} , Country: {weather.sys?.country}
+            </p>
+            <p>Temperature: {weather.main?.temp} °C</p>
+            <p>
+              Weather:{' '}
+              {Array.isArray(weather.weather) && weather.weather[0]?.main}
+            </p>
+            <p>Wind: {weather.wind?.speed} m/s</p>
+            <p>Humidity: {weather.main?.humidity} %</p>
+            <p>Longitude: {weather.coord?.lon}</p>
+            <p>Latitude: {weather.coord?.lat}</p>
+          </div>
+        )}
 
-      {!tempErrorMessage && temperatures && (
-        <div className="weather-data">
-          {temperatures.list.slice(0, 3).map((item, index) => (
-            <div key={index}>
-              <p>Forecast time: {new Date(item.dt * 1000).toLocaleString()}</p>
-              <p>Temperature: {item.main.temp} °C</p>
-              <p>Weather: {item.weather[0].main}</p>
-            </div>
-          ))}
-        </div>
-      )}
+        {errorMessage && (
+          <div className="error-message">
+            <span>{error}</span>
+          </div>
+        )}
+
+        {!tempErrorMessage && temperatures?.list && (
+          <div className="weather-data">
+            Temperatures:
+            {temperatures.list.slice(0, 3).map((item, index) => (
+              <div key={index}>
+                <p>
+                  Forecast time: {new Date(item.dt * 1000).toLocaleString()}
+                </p>
+                <p>Temperature: {item.main?.temp} °C</p>
+                <p>Weather: {item.weather?.[0]?.main}</p>
+              </div>
+            ))}
+          </div>
+        )}
+
+        {tempErrorMessage && (
+          <div className="error-message">
+            <span>{tempError}</span>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
